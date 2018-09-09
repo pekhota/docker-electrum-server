2359fa008c4e57d751af1a3f5b74bfdc25491d98
Pekhota <pekhota.alex@gmail.com>
2018-09-09 14:30:46 +0300

    Fixed bug with network at electrum spawn

--- load-wallet-with-password.sh
+++ load-wallet-with-password.sh
@@ -3,7 +3,13 @@
 #https://www.thegeekstuff.com/2010/10/expect-examples/
 set password [lindex $argv 0]
 set network [lindex $argv 1]
-spawn electrum ${network} -D /app/electrum daemon load_wallet
+
+if {$network eq ""} {
+    spawn electrum -D /app/electrum daemon load_wallet
+} else {
+    spawn electrum ${network} -D /app/electrum daemon load_wallet
+}
+
 expect "Password:"
 send "$password\r";
 expect "true"
