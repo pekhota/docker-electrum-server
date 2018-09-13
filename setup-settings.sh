3825b4b24e3bc0cb3327241dd2c0b4bc128a21d1
Pavel Movchan <ukrolove@gmail.com>
2018-09-13 15:04:43 +0300

    Deleted the odd files

--- setup-settings.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#https://stackoverflow.com/questions/5750450/print-each-command-before-executing/5750463#5750463
-set -o xtrace
-
-electrum ${ELECTRUM_ENV_NETWORK_TYPE} -D /app/electrum setconfig rpchost 0.0.0.0
-electrum ${ELECTRUM_ENV_NETWORK_TYPE} -D /app/electrum setconfig rpcport 7777
-electrum ${ELECTRUM_ENV_NETWORK_TYPE} -D /app/electrum setconfig rpcuser user
-electrum ${ELECTRUM_ENV_NETWORK_TYPE} -D /app/electrum setconfig rpcpassword ssow6Qc-kKwycQ_1SeVXVQ==
