932577c93ab59dec1a7300c7ea4aeb13012c1235
Pekhota <pekhota.alex@gmail.com>
2018-09-18 16:27:38 +0300

    Removed code with bugs; Fixed healthcheck script for python exit code;

--- el-scripts/healthcheck.sh
+++ el-scripts/healthcheck.sh
@@ -9,7 +9,8 @@ URL="${ELECTRUM_RPC_HOST}:${ELECTRUM_RPC_PORT}"
 while true; do
     echo "[$(date)] Running check script..."
 
-    python3 /el-scripts/check.py -url=${URL} --login=${ELECTRUM_RPC_USER} --pwd=${ELECTRUM_RPC_PASSWORD}
+    python3 /el-scripts/check.py --url=${URL} --login=${ELECTRUM_RPC_USER} --pwd=${ELECTRUM_RPC_PASSWORD}
+    [ $? -ne 0 ] && echo -e "\n**** ---- Failed to ping electrum. ****\n" && exit 1
 
     sleep 3
 done
